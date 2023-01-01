b6d70393cca3ddeaaf5116f932fdedcdfa14d3f8
Ferenc-Varga <ferenc.varga@student.hepl.be>
2023-01-01 20:15:46 +0100

    Amélioration Fonctionnalités V2

--- lib/Info_Recette.dart
+++ lib/Info_Recette.dart
@@ -8,6 +8,34 @@ class InfoRecette extends StatefulWidget {
 }
 
 class _InfoRecette extends State<InfoRecette> {
+  final List<String> _contenus = [    "Contenu du conteneur 1",    "Contenu du conteneur 2",    "Contenu du conteneur 3",  "Contenu du conteneur 4",    "Contenu du conteneur 5",    "Contenu du conteneur 6", "Contenu du conteneur 7",    "Contenu du conteneur 8",    "Contenu du conteneur 9", "Contenu du conteneur 10",    "Contenu du conteneur 11",    "Contenu du conteneur 12", "Contenu du conteneur 13",    "Contenu du conteneur 14",    "Contenu du conteneur 15", "Contenu du conteneur 16",    "Contenu du conteneur 17",    "Contenu du conteneur 18"];  // Ajoutez ici les autres éléments du tableau
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Info Recette Page'),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        actions: const <Widget>[
+          // Add widgets here for the different features of your app bar
+        ],
+      ),
+      body: Center(
+        child: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: Column(
+            children: List.generate(18, (index) => createContainer(index + 1)),
+          ),
+        ),
+      ),
+    );
+  }
+
   Widget createContainer(int index) {
     return Column(
       children: [
@@ -32,6 +60,15 @@ class _InfoRecette extends State<InfoRecette> {
           ),
           child: InkWell(
             onTap: () {
+              showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return AlertDialog(
+                    title: Text("Conteneur $index"),
+                    content: Text(_contenus[index - 1]),
+                  );
+                },
+              );
             },
             child: Image.network(
               'https://www.pleinevie.fr/wp-content/uploads/pleinevie/2021/06/surcharger-refrigerateur.jpeg',
@@ -43,30 +80,6 @@ class _InfoRecette extends State<InfoRecette> {
       ],
     );
   }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Info Recette Page'),
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back),
-          onPressed: () {
-            Navigator.pop(context);
-          },
-        ),
-        actions: const <Widget>[
-          // Add widgets here for the different features of your app bar
-        ],
-      ),
-      body: Center(
-        child: SingleChildScrollView(
-          scrollDirection: Axis.vertical,
-          child: Column(
-            children: List.generate(18, (index) => createContainer(index)),
-          ),
-        ),
-      ),
-    );
-  }
 }
+
+  createContainer(int index) {}
